49096b9759acabf21875cbe0b8ea2830310a4536
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-18 14:54:04 +0600

    modified

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,13 +1,19 @@
+import React from "react";
+
 const ListItems = (props) => {
   return (
-    <li className="">
+    <li>
       <div className="task">
-        <span className="mr-3 cursor-pointer">
-          <i className="fa-regular fa-circle"></i>
+        <span onClick={() => props.onComplete(props.id)} className="mr-3 cursor-pointer">
+          {props.completed ? (
+            <i className="fa-regular fa-circle-check"></i>
+          ) : (
+            <i className="fa-regular fa-circle"></i> 
+          )}
         </span>
         <span>{props.value}</span>
       </div>
-      <span onClick={ () => props.onSelect(props.id)} className="cross">
+      <span onClick={() => props.onSelect(props.id)} className="cross">
         <i className="fa-solid fa-xmark"></i>
       </span>
     </li>
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ListItems from "./ListItems";
 
 const TodoApp = () => {
-  const [display, setDisplay] = useState();
+  const [display, setDisplay] = useState("");
   const [items, setItems] = useState([]);
 
   const showData = (e) => {
@@ -11,33 +11,43 @@ const TodoApp = () => {
 
   const addItems = () => {
     setItems((oldItems) => {
-      return [...oldItems, display];
+      return [...oldItems, { text: display, completed: false }];
     });
+    setDisplay(""); 
   };
 
   const deleteItem = (id) => {
-
     setItems((oldItems) => {
       return oldItems.filter((arrEle, index) => {
         return index !== id;
-      })
-    })
-}
+      });
+    });
+  };
 
+  const completeItem = (id) => {
+    setItems((oldItems) => {
+      return oldItems.map((item, index) => {
+        if (index === id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      });
+    });
+  };
 
   return (
     <div>
       <div className="container">
         <div className="flex h-screen w-full justify-center p-6">
           <div className="box max-w-sm w-full h-fit bg-white rounded-md p-4">
-            <h1 className=" font-bold text-center text-2xl font-poppins">
-              To Do Application <br /> by{" "}
-              <span className="text-[#61DBFB]">React</span>
+            <h1 className="font-bold text-center text-2xl font-poppins">
+              To Do Application <br /> by <span className="text-[#61DBFB]">React</span>
             </h1>
             <div className="input text-center mt-8 flex gap-2">
               <input
                 onChange={showData}
-                className=" flex-1 h-10 p-3 rounded-lg border border-[#61DBFB] focus:outline-none"
+                value={display} 
+                className="flex-1 h-10 p-3 rounded-lg border border-[#61DBFB] focus:outline-none"
                 type="text"
                 placeholder="Input your task..."
               />
@@ -48,10 +58,17 @@ const TodoApp = () => {
 
             <div className="tasks mt-8">
               <ul>
-                {/* <ListITems/> */}
-
                 {items.map((taskItem, index) => {
-                  return <ListItems key={index} id={index} onSelect={deleteItem} value={taskItem} />;
+                  return (
+                    <ListItems
+                      key={index}
+                      id={index}
+                      onSelect={deleteItem}
+                      onComplete={completeItem}
+                      value={taskItem.text}
+                      completed={taskItem.completed}
+                    />
+                  );
                 })}
               </ul>
             </div>
